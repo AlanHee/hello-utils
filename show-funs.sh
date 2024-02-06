c9d3456106ee1d666e1f13193dfcf857b04f184f
AlanHee <alan.hee@outlook.com>
2024-02-06 22:54:40 +0800

    Update show-funs

--- show-funs.sh
+++ show-funs.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-cmds=(cmatrix, neofetch)
+cmds=(cmatrix neofetch)
 
-for i in ${cmds[@]}; do
-	echo $i
+echo "Funs list:"
+for i in "${!cmds[@]}"; do
+	echo "$((i + 1)). ${cmds[$i]}"
 done
+
+read -p "Choose in: " choice
+
+exec "${cmds[choice - 1]}"
